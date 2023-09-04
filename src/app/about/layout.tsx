de3f9db15c75aa6490b59417f7eebd87f3a5b5b0
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-09-04 23:54:20 +0530

    last deployment trial , I changed the cases of components

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,9 +1,9 @@
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 export const metadata: Metadata = {
   title: 'ToDo List',
   description: 'Created w/ NextJs',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-page-custom-font */
 "use client";
 import Image from 'next/image';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import styled, { createGlobalStyle } from "styled-components";
-import Footer from './components/footer';
-import TaskForm from './components/form';
-import TaskList from './components/list';
+import Footer from './components/Footer';
+import TaskForm from './components/Form';
+import TaskList from './components/List';
 import { useEffect, useState } from 'react';
-import Intro from './components/intro';
+import Intro from './components/Intro';
 const GlobalStyle = createGlobalStyle`
 @font-face {
   font-family: 'Ambit';
