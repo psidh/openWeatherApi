d6c960fad393f4046939889344cc832cd97479e9
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-09-05 00:45:24 +0530

    v1

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -23,7 +23,7 @@ const TaskForm: React.FC<TaskFormProps> = ({ addTask }) => {
         placeholder="Add Task"
         value={task}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTask(e.target.value)}
-        className="dark:bg-white dark:text-black py-3 pl-4 pr-28 rounded-lg border border-gray-300"
+        className="dark:bg-black dark:text-white py-3 pl-4 pr-28 rounded-lg border border-gray-300"
         />
       <button type="submit" className="text-xl md:text-2xl mt-4 py-2 px-12  hover:bg-teal-600 bg-teal-500 text-white rounded-md">
         Add
--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -31,11 +31,11 @@ const TaskList: React.FC<TaskListProps>  = ({ tasks, deleteTask, toggleComplete,
 
   return ( 
 <div className="mx-4">
-  <ul className="my-4 space-y-4">
+  <ul className="my-4 space-y-4  md:h-[50vh]">
     {tasks.map((task, index) => (
       <li
         key={index}
-        className="bg-white p-4 border shadow-md rounded-lg hover:shadow-lg mx-auto max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl"
+        className="dark:bg-black   bg-white p-4 border shadow-md rounded-lg hover:shadow-lg mx-auto max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl"
       >
         <div className="flex flex-col sm:flex-row items-center justify-between">
           <div className="flex items-center space-x-2">
@@ -43,7 +43,7 @@ const TaskList: React.FC<TaskListProps>  = ({ tasks, deleteTask, toggleComplete,
               type="checkbox"
               checked={task.completed}
               onChange={() => toggleComplete(index)}
-              className={`h-6 w-6 text-white border border-gray-200 rounded-full focus:ring-2 focus:ring-blue-400 ${
+              className={`h-6 w-6  border border-gray-200 rounded-full focus:ring-2 focus:ring-blue-400 ${
                 task.completed
                   ? 'bg-yellow-500 border-yellow-500'
                   : 'bg-gray-200 border-gray-200'
@@ -59,7 +59,7 @@ const TaskList: React.FC<TaskListProps>  = ({ tasks, deleteTask, toggleComplete,
                 />
               ) : (
                 <span
-                  className={`text-base ${
+                  className={`text-base dark:text-white text-black ${
                     task.completed ? 'line-through text-gray-400' : 'text-black'
                   }`}
                 >
@@ -72,21 +72,21 @@ const TaskList: React.FC<TaskListProps>  = ({ tasks, deleteTask, toggleComplete,
             {editMode === index ? (
               <button
                 onClick={() => handleSaveClick(index)}
-                className="text-sm bg-green-600 hover:bg-green-700 text-white px-2 py-1 rounded-md hover:text-gray-300 focus:outline-none mb-2 sm:mb-0"
+                className="text-sm bg-green-600 hover:bg-green-700  text-white px-2 py-1 rounded-md hover:text-gray-300 focus:outline-none mb-2 sm:mb-0"
               >
                 Save
               </button>
             ) : (
               <button
                 onClick={() => handleEditClick(index, task.name)}
-                className="text-sm bg-blue-600 hover:bg-blue-700 text-white px-2 py-1 rounded-md hover:text-gray-300 focus:outline-none mb-2 sm:mb-0"
+                className="text-sm bg-blue-600 hover:bg-blue-700  text-white px-2 py-1 rounded-md hover:text-gray-300 focus:outline-none mb-2 sm:mb-0"
               >
                 Edit
               </button>
             )}
             <button
               onClick={() => deleteTask(index)}
-              className="text-sm bg-red-600 hover:bg-red-700 text-white px-2 py-1 rounded-md hover:text-gray-300 focus:outline-none mb-2 sm:mb-0"
+              className="text-sm bg-red-600 hover:bg-red-700  text-white px-2 py-1 rounded-md hover:text-gray-300 focus:outline-none mb-2 sm:mb-0"
             >
               Delete
             </button>
