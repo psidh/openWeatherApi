69bebc6e72bfb9bc55f3622bddd140b3726b8171
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-09-05 16:05:36 +0530

    weather

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,8 @@
 import Image from 'next/image';
 import styled, { createGlobalStyle } from "styled-components";
 import { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
